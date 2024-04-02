93bc10487ab97bd168220debc825a68b20c267a8
Baris Anik <31242517+barisanik@users.noreply.github.com>
2024-04-02 03:55:25 +0100

    Updates on the writing test
    - Developed a real-time based input system to see writing of user with keyboard library.
    - Created a temprorary scoring system
    - Calculated total duration of the test.

--- Python-Practices/writing-test.py
+++ Python-Practices/writing-test.py
@@ -2,27 +2,62 @@
 # It will give a random word and control every input character and will mark every correct letters and mistakes. 
 # It will record the time as well to print at the end of test. There may be effect of time for marking in the future.
 
-import random, datetime, keyboard
+def analyzeInput(targetWord):
+    import keyboard
+    inputWord, char_event, char = "", "", ""
+    inputCharIndex, score, correctTypeScore, falseTypeScore = 0,0,0,0
 
-level = 0
-wordCount = 3 # Word count will be standardized after having a enough word list.
+    while True:
+        char_event = keyboard.read_event(suppress=True)
+        if char_event.event_type != keyboard.KEY_UP:
+            continue
+
+        char = char_event.name
+
+        if char == "enter":
+            break
+        elif char == "backspace":
+            if inputWord:
+                inputWord = inputWord[:-1]
+                print("\r{}".format(inputWord), end="")
+        else:
+            if char == targetWord[inputCharIndex]:
+                inputCharIndex += 1
+                inputWord += char
+                print("\r{}".format(inputWord), end="")
+                score += 10
+                correctTypeScore += 1
+            else:
+                score -= 10
+                falseTypeScore =+ 1
+        if inputWord == targetWord:
+            print("\r{}".format(inputWord), end="")
+            return [score, correctTypeScore, falseTypeScore]
+        char_event, char = "",""
+
+import random, datetime
+
+level = 0 # Level var. will be used after having an enough word list.
+wordCount = 3 # Word count will be standardized after having an enough word list.
 chosenWord = ""
 inputWord = ""
-startTime, finishTime, wordStartTime, wordFinishTime, point, correctWords, wrongWords = 0,0,0,0,0,0,0
+startTime, finishTime, wordStartTime, wordFinishTime, score, correctWords, wrongWords = 0,0,0,0,0,0,0
+totalCorrectTypeScore, totalFalseTypeScore = 0,0
 
-wordList = ["car","rain","building","comprehensive","time"]
+wordList = ["car","rain","time"]
 startTime = datetime.datetime.now()
 while wordCount > 0:
     chosenWord = random.choice(wordList)
-    print(10*" " + chosenWord + 10 * " ")
-    inputWord = input("Please write the word above: ")
-    if inputWord != chosenWord:
-        point -= 100
-        wrongWords += 1
-    else:
-        point += 100
-        correctWords += 1
+    print("\n" + 10 * " " + chosenWord + 10 * " ")
+    print("Please write the word above: ")
+    outputs = analyzeInput(chosenWord)
+    score += outputs[0]
+    totalCorrectTypeScore += outputs[1]
+    totalFalseTypeScore += outputs[2]
     wordCount -= 1
 finishTime = datetime.datetime.now()
 
-print("You wrote {} correct words and {} false one. Final point: {}".format(correctWords, wrongWords, point))
+print("""\n\tRESULTS\n{}\nCorrect typing: {}
+False Typing: {}
+Test Completion Time: {}
+Final Score: {}""".format(25*"-",totalCorrectTypeScore, totalFalseTypeScore, finishTime-startTime, score))
